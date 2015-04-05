ade356a4dac756f9631bb04fe62d5731c23ab176
ivelintod <benitezzz@abv.bg>
2015-04-06 01:51:45 +0300

    Delete IVAN.py

--- week1/IVAN.py
+++
@@ -1,8 +0,0 @@
-def is_prime(number):
-     divisors = [x for x in range(2, number) if number % x == 0]
-     if divisors == []:
-        return "Prime"
-     else:
-        return "Not prime"
-
-print is_prime(18)
